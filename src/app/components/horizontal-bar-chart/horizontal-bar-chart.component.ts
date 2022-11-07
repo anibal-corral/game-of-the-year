4ea78210373ce16f4aa3ff50a0ad70415f31b295
Anibal Corral <anibal.corral@gmail.com>
2022-11-07 11:40:56 -0500

    Initial chart working

--- src/app/components/horizontal-bar-chart/horizontal-bar-chart.component.ts
+++ src/app/components/horizontal-bar-chart/horizontal-bar-chart.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+// import { single } from './data';
 @Component({
   selector: 'app-horizontal-bar-chart',
   templateUrl: './horizontal-bar-chart.component.html',
   styleUrls: ['./horizontal-bar-chart.component.css']
 })
 export class HorizontalBarChartComponent implements OnInit {
+  result: any[]=[
+    {
+      "name": "Game 1",
+      "value": 10
+    },
+    {
+      "name": "Game 2",
+      "value": 20
+    },
+    {
+      "name": "Game 3",
+      "value": 5
+    },
+    {
+      "name": "Game 4",
+      "value": 8
+    }
+  ];
+  view = [700, 400];
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = false;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  yAxisLabel: string = 'Games';
+  showYAxisLabel: boolean = true;
+  xAxisLabel: string = 'Votes';
+
+  colorScheme = 'nightLights'
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+
+  onSelect(data:any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data:any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data:any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
